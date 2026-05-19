b260a6cdd744fb4f7b24bb8ac8f32abda3571fa2
fallabdoulaye93 <fallabdoulaye93@users.noreply.github.com>
2026-05-20 00:31:04 +0200

    Add debug logs

--- index.js
+++ index.js
@@ -5,14 +5,46 @@ const app = express();
 
 app.get('/', async (req, res) => {
   try {
-    const response = await axios.get(
-        'http://s1.pluton-pro.com/get.php?username=5159228492&password=0318872656&output=ts&type=m3u_plus'
-    );
 
+    const url =
+        'http://s1.pluton-pro.com/get.php?username=5159228492&password=0318872656&output=ts&type=m3u_plus';
+
+    console.log('Calling:', url);
+
+    const response = await axios.get(url, {
+      timeout: 20000,
+      responseType: 'text',
+      headers: {
+        'User-Agent': 'Mozilla/5.0'
+      }
+    });
+
+    console.log('SUCCESS');
+
+    res.type('text/plain');
     res.send(response.data);
+
   } catch (e) {
-    res.status(500).send(e.toString());
+
+    console.log('ERROR FULL:', e);
+
+    if (e.response) {
+      console.log('STATUS:', e.response.status);
+      console.log('DATA:', e.response.data);
+    }
+
+    if (e.code) {
+      console.log('CODE:', e.code);
+    }
+
+    res.status(500).send({
+      message: e.message,
+      code: e.code,
+      stack: e.stack
+    });
   }
 });
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Server started');
+});
